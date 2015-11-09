8bb3b1767ceb3adca8db21b868c06143c4fb26dd
mthsena <mthsena@gmail.com>
2015-11-09 20:38:11 -0200

    Fix openwow tooltip game version

--- wcshop/views/panel.php
+++ wcshop/views/panel.php
@@ -22,7 +22,7 @@ require_once "handlers/panel.php";
         <?php foreach($items as $item) : ?>
         <tr>
             <td>
-                <a href="http://cata.openwow.com/item=<?php echo $item['id']; ?>"><?php echo $item["name"]; ?></a>
+                <a href="http://wotlk.openwow.com/item=<?php echo $item['id']; ?>"><?php echo $item["name"]; ?></a>
             </td>
             <td id="price">
                 <?php echo $item["price"]; ?>
